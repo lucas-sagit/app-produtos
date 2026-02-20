203036d11865f68d0cae98550d17f66cfffc60f8
Lucas <lv011297@gmail.com>
2026-02-20 16:47:27 -0300

    feat: add component, route, img

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './shared/components/component/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'products', component:ProductsComponent}
+];
--- frontend/src/app/shared/components/component/products.component.spec.ts
+++ frontend/src/app/shared/components/component/products.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductComponent } from './product-component';
+import { ProductsComponent } from './products-component';
 
-describe('ProductComponent', () => {
-  let component: ProductComponent;
-  let fixture: ComponentFixture<ProductComponent>;
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductComponent]
+      imports: [ProductsComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProductComponent);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
